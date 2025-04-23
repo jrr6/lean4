64c069342df0993ad8ce1f9671d67cecd0f6246f
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-04-23 23:27:34 +0200

    WIP application and structure projection errors

--- tests/lean/unsound.lean
+++ tests/lean/unsound.lean
@@ -1,3 +1,10 @@
+def Exists.witness {p : α → Prop} : (∃ x : α, p x) → α
+  | Exists.intro w h => w
+
+structure Existy {α : Sort u} (p : α → Prop) : Prop where
+  w : α
+  h : p w
+
 def foo (h : ∃ x: Nat, True) := h.1
 theorem contradiction : False :=
   (by decide : 0 ≠ 1) (show foo ⟨0, trivial⟩ = foo ⟨1, trivial⟩ from rfl)
