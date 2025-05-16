2bb0c3b97fc1a4455b8f197d4be70d9fc14e004b
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-05-17 00:31:02 +0200

    WIP formatting improvements

--- src/Lean/Message.lean
+++ src/Lean/Message.lean
@@ -638,9 +638,9 @@ def inlineExpr (e : Expr) (maxInlineLength := 30) : MessageData :=
       if (← msg.formatLength ctx) > maxInlineLength then
         return indentD msg ++ "\n"
       else
-        return " " ++ msg ++ " ")
+        return " `" ++ msg ++ "` ")
     (fun mctx => instantiateMVarsCore mctx e |>.1.hasSyntheticSorry)
-    (fun () => return " " ++ MessageData.ofExpr e ++ " ")
+    (fun () => return " `" ++ MessageData.ofExpr e ++ "` ")
 
 /-- Atom quotes -/
 def aquote (msg : MessageData) : MessageData :=
