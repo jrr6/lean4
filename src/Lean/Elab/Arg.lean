e758b623456effc1047fe5bac4894c68f64a929d
Joseph Rotella <7482866+jrr6@users.noreply.github.com>
2025-06-12 17:32:14 +0200

    chore: minor cleanup and wording adjustments

--- src/Lean/Elab/Arg.lean
+++ src/Lean/Elab/Arg.lean
@@ -29,15 +29,15 @@ structure NamedArg where
 
 instance : ToMessageData Arg where
   toMessageData
-  | .stx stx => .ofSyntax stx
-  | .expr e => .ofExpr e
+  | .stx stx => toMessageData stx
+  | .expr e  => toMessageData e
 
 /--
   Add a new named argument to `namedArgs`, and throw an error if it already contains a named argument
   with the same name. -/
 def addNamedArg (namedArgs : Array NamedArg) (namedArg : NamedArg) : MetaM (Array NamedArg) := do
   if namedArgs.any (namedArg.name == ·.name) then
-    withRef namedArg.ref <| throwError "Argument '{namedArg.name}' was already set"
+    throwErrorAt namedArg.ref "Argument '{namedArg.name}' was already set"
   return namedArgs.push namedArg
 
 partial def expandArgs (args : Array Syntax) : MetaM (Array NamedArg × Array Arg × Bool) := do
